5bf2a5daa396f9c98140e0776cc96e04b38fab1f
Ivan <imivanfarias@gmail.com>
2023-01-08 11:17:13 -0300

    Changes to be committed: 	modified:   src/components/Header.astro 	modified:   src/components/Navigation.astro 	modified:   src/layouts/BaseLayout.astro 	modified:   src/pages/about.astro 	new file:   src/scripts/themeColor.js 	modified:   src/styles/global.css

---
+++ src/scripts/themeColor.js
@@ -0,0 +1,28 @@
+let colorThemes = document.querySelectorAll('[name="theme"]');
+
+// store theme
+  let storeTheme = function (theme) {
+  localStorage.setItem("theme", theme);
+};
+
+// set theme when visitor returns
+  let setTheme = function () {
+  let activeTheme = localStorage.getItem("theme");
+  colorThemes.forEach((themeOption) => {
+    if (themeOption.id === activeTheme) {
+      themeOption.checked = true;
+    }
+  });
+  // fallback for no :has() support
+  document.documentElement.className = activeTheme;
+};
+
+colorThemes.forEach((themeOption) => {
+  themeOption.addEventListener("click", () => {
+    storeTheme(themeOption.id);
+    // fallback for no :has() support
+    document.documentElement.className = themeOption.id;
+  });
+});
+
+document.onload = setTheme();

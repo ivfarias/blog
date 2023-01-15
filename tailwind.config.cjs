e082e9eb0dec342f11e15871550d90ea6d3e9e96
Ivan <imivanfarias@gmail.com>
2023-01-15 15:56:12 -0300

    Changes to be committed: 	modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   public/img/noise-grad-byr-1.svg 	new file:   public/img/sm-noise.png 	new file:   public/img/sm-squared-blk-1.svg 	modified:   src/components/BlogPost.astro 	modified:   src/components/Header.astro 	new file:   src/components/Hero.astro 	modified:   src/components/Navigation.astro 	new file:   src/components/SectionDivider.astro 	modified:   src/layouts/BaseLayout.astro 	modified:   src/layouts/MarkdownPostLayout.astro 	modified:   src/pages/index.astro 	modified:   src/styles/global.css 	modified:   tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,34 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		backgroundImage: {
+			'noise': "url('./public/img/sm-noise.png')",
+			'sm-squared-blk-1': "url('./public/img/sm-squared-blk-1.svg')",
+			'noise-grad-byr-1': "url('./public/img/noise-grad-byr-1.svg')"
+		},
+		colors: {
+			'riso-black-1': '#31312B',
+			'riso-black-2': '#5A5A55',
+			'riso-black-3': '#838380',
+			'riso-black-4': '#ADADAA',
+			'riso-blue-1': '#3765AB',
+			'riso-blue-2': '#5F84BC',
+			'riso-blue-3': '#87A3CD',
+			'riso-blue-4': '#AFC1DD',
+			'riso-pink-1': '#FE67BC',
+			'riso-pink-2': '#FE85C9',
+			'riso-pink-3': '#FEA4D7',
+			'riso-pink-4': '#FFC2E4',
+			'riso-yellow-1': '#F8ED3B',
+			'riso-yellow-2': '#F9F162',
+			'riso-yellow-3': '#FBF489',
+			'riso-yellow-4': '#FCF8B1',
+			'riso-red-1': '#F25050',
+			'riso-red-2': '#F57373',
+			'riso-red-3': '#F79696',
+			'riso-red-4': '#FAB9B9',
+			'kinda-white': '#F3EDDE'
+		},
 		screens: {
 			'sm': '640px',
 			// => @media (min-width: 640px) { ... }
@@ -21,6 +49,6 @@ module.exports = {
 		extend: {},
 	},
 	plugins: [
-		require('@tailwindcss/typography'),
+		require('@tailwindcss/typography', '@tailwindcss/forms'),
 ],
 }

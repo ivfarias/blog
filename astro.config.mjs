64cffdeba8f85b6ed3e526ca39fa1ffdd2d41b5c
Ivan <imivanfarias@gmail.com>
2023-01-16 11:24:28 -0300

    Changes to be committed: 	modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   src/components/Breadcrumbs.astro 	modified:   src/components/Navigation.astro 	modified:   src/layouts/MarkdownPostLayout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()]
+  integrations: [tailwind(), robotsTxt(), svelte()]
 });
